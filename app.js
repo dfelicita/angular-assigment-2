cfbcf96fdf65c42541b3cade255b335a0de1b751
Daniel Felicita <daniel.felicita@gmail.com>
2017-09-20 10:05:25 -0500

    no message

--- app.js
+++ app.js
@@ -2,22 +2,20 @@
 'use strict';
 
 angular.module('ControllerAsApp', [])
-.controller('ShoppingListController', ShoppingListController)
-// .controller('ShoppingListController2', ShoppingListController2)
+.controller('ShoppingListController1', ShoppingListController1)
+.controller('ShoppingListController2', ShoppingListController2)
 .factory('ShoppingListFactory', ShoppingListFactory);
 
 // LIST #1 - controller
-ShoppingListController.$inject = ['ShoppingListFactory'];
-function ShoppingListController(ShoppingListFactory) {
-  $scope.list1 = [];
-  $scope.list2 = [];
-
+ShoppingListController1.$inject = ['ShoppingListFactory'];
+function ShoppingListController1(ShoppingListFactory) {
+  var list1 = this;
+  
   // Use factory to create new shopping list service
   var shoppingList1 = ShoppingListFactory();
+  
   list1.items = shoppingList1.getItems();
-  var shoppingList2 = ShoppingListFactory();
-  list2.items = shoppingList2.getItems();
-
+  
   list1.addItem = function (name, quantity) {
     shoppingList1.addItem(name, quantity);
   };
@@ -25,46 +23,29 @@ function ShoppingListController(ShoppingListFactory) {
   list1.removeItem = function (itemIndex) {
     shoppingList1.removeItem(itemIndex);
   };
-  list2.addItem = function (name, quantity) {
-    shoppingList2.addItem(name, quantity);
-  };
-
-  list2.removeItem = function (itemIndex) {
-    shoppingList2.removeItem(itemIndex);
-  };
-
-  list1.addItem('Cookies', 100);
-  list1.addItem('Sodas', 12);
-  list1.addItem('Beers', 24);
-
-  console.log('LISTS', list1, list2);
+  
   
 }
 
 
 // LIST #2 - controller
-// ShoppingListController2.$inject = ['ShoppingListFactory'];
-// function ShoppingListController2(ShoppingListFactory) {
-//   var list2 = this;
+ShoppingListController2.$inject = ['ShoppingListFactory'];
+function ShoppingListController2(ShoppingListFactory) {
+  var list2 = this;
 
-//   // Use factory to create new shopping list service
-//   var shoppingList = ShoppingListFactory();
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory();
 
-//   list2.items = shoppingList.getItems();
+  list2.items = shoppingList.getItems();
 
-//   list2.addItem = function () {
-//     try {
-//       shoppingList.addItem(list2.itemName, list2.itemQuantity);
-//     } catch (error) {
-//       list2.errorMessage = error.message;
-//     }
+  list2.addItem = function (name, quantity) {
+    shoppingList.addItem(name, quantity);
+  }
 
-//   }
-
-//   list2.removeItem = function (itemIndex) {
-//     shoppingList.removeItem(itemIndex);
-//   };
-// }
+  list2.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
+  };
+}
 
 
 // If not specified, maxItems assumed unlimited
